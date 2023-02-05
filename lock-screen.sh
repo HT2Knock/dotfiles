e661d12fe52646b8383143cf9b1be26e1990b313
AustinHuynh248 <huynhtransongngoc@gmail.com>
2023-02-05 17:18:40 +0700

    Add alacritty, tmux, zsh

---
+++ lock-screen.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+BLANK='#3b4252'
+CLEAR='#434c5e'
+DEFAULT='#2e3440'
+TEXT='#81a1c1'
+WRONG='#5e81ac'
+WRONG_TEXT='#bf616a'
+VERIF_TEXT='#8fbcbb'
+VERIFYING='#d8dee9'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$VERIF_TEXT    \
+--wrong-color=$WRONG_TEXT    \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--wrong-text="XXX"           \
+--verif-text="..."           \
+\
+--wrong-font="JetBrains Mono" \
+--verif-font="JetBrain Mono"  \
+--time-font="JetBrains Mono"  \
+--date-font="JetBrains Mono"  \
+--screen 1                   \
+--blur 5                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M"           \
+--date-str="%Y-%m-%d"       \
