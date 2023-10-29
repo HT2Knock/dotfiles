a48fc35fbf476b27d8ff7d1ca8bf2685a0ee44ec
AustinHuynh248 <huynhtransongngoc@gmail.com>
2023-10-29 17:17:02 +0700

    feat: change wallpaper config

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SOURCE_DIR="/home/cubable-be-4/Pictures/walls"
+SOURCE_DIR="$HOME/Pictures/walls"
 
 category_path="$(find $SOURCE_DIR -maxdepth 1 -type d -not -name "animated" -and -not -name ".*" | shuf -n 1)"
 
 wall_paper=$(find "$category_path" -not -name "*.md" -and -not -name ".*" | shuf -n 1)
 
-feh "$wall_paper" --bg-scale
+feh --bg-scale "$wall_paper"
