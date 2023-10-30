31a5914b8ff90dd5dc0faa57bf4f52c9c073923c
ngochuynh <huynhtransongngoc@gmail.com>
2023-10-30 09:14:42 +0700

    feat: remvoe change

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -6,4 +6,4 @@ category_path="$(find $SOURCE_DIR -maxdepth 1 -type d -not -name "animated" -and
 
 wall_paper=$(find "$category_path" -not -name "*.md" -and -not -name ".*" | shuf -n 1)
 
-feh "$wall_paper" --bg-scale
+feh --bg-scale "$wall_paper"
