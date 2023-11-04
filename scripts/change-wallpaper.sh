8ad2391e9c50184a7e65cebe22b9507d3530839a
AustinHuynh248 <huynhtransongngoc@gmail.com>
2023-11-04 21:30:33 +0700

    chore: quote that thing

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -2,7 +2,7 @@
 
 SOURCE_DIR="/home/cubable-be-4/Pictures/walls"
 
-category_path="$(find $SOURCE_DIR -maxdepth 1 -type d -not -name "animated" -and -not -name ".*" | shuf -n 1)"
+category_path="$(find "$SOURCE_DIR" -maxdepth 1 -type d -not -name "animated" -and -not -name ".*" | shuf -n 1)"
 
 wall_paper=$(find "$category_path" -not -name "*.md" -and -not -name ".*" | shuf -n 1)
 
