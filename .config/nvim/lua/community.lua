e2eb3214836b7daef0b3cc75ea5cc0d1364f0daf
ngochuynh <huynhtransongngoc@gmail.com>
2024-04-11 09:31:41 +0700

    feat(nvim): add refactoring nvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -4,4 +4,5 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.lsp.nvim-lsp-file-operations" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
 }
