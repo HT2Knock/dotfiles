165a6aecfc95bc72a1442e0130a13633d915a782
ngochuynh <huynhtransongngoc@gmail.com>
2024-05-13 10:34:43 +0700

    fix(script): correct change wallpaper script

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -2,12 +2,23 @@
 
 SOURCE_DIR="$HOME/Pictures/aesthetic-wallpapers/images"
 
-pushd "$SOURCE_DIR" >/dev/null || exit 1
+# Check if SOURCE_DIR exists
+if [ ! -d "$SOURCE_DIR" ]; then
+	echo "Error: Source directory $SOURCE_DIR not found." >&2
+	exit 1
+fi
 
 category_dir="$(find "${SOURCE_DIR}" -type -f | shuf -n 1)"
 
-pushd "$category_dir" >/dev/null || exit 1
+# Select a random image file
+while IFS= read -r -d '' img_name; do
+	# Set desktop background
+	feh --bg-scale "$img_name"
+	break # Exit after setting the background once
+done < <(find . \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | shuf -zn 1)
 
-img_name=$(ls *.{png,jpg,jpeg} | shuf -n 1)
-
-feh --bg-fill "${img_name}"
+# Check if img_name is empty
+if [ "$img_name" = "" ]; then
+	echo "Error: No image files found in $SOURCE_DIR." >&2
+	exit 1
+fi
