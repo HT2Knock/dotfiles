3ca879948ca1bedb4313c2442ef28c5e1dd9ba05
ngochuynh <huynhtransongngoc@gmail.com>
2024-06-06 16:39:30 +0700

    feat(nvim:) using community trouble

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -3,5 +3,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
 }
