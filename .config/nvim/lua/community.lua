dfa483238b97e16c6acf55df6ff8e295f9a292e7
ngochuynh <huynhtransongngoc@gmail.com>
2024-06-11 19:21:33 +0700

    feat(nvim): add noice nvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -3,6 +3,8 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.project.nvim-spectre" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
 }
