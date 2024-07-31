fe79eaeaeb4b373955be0364a517087b42b3a367
ngochuynh <huynhtransongngoc@gmail.com>
2024-07-31 10:19:54 +0700

    chore(nvim): update astro and stop using noice

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -3,7 +3,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.search.nvim-spectre" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
