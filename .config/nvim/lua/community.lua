dd253caf1de86892868a9a7e7bc02a10374a15a8
knock-huynh <huynhtransongngoc@gmail.com>
2024-09-07 21:57:21 +0700

    feat(nvim): add go package to nvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -3,6 +3,8 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.search.nvim-spectre" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
