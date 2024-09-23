f0c623585c1e1befe0e9ea333d40cdfe75c1ac79
ngochuynh <huynhtransongngoc@gmail.com>
2024-09-23 09:37:08 +0700

    feat(nvim): add markview as markdown render

--- .config/nvim/lua/plugins/note.lua
+++ .config/nvim/lua/plugins/note.lua
@@ -1,9 +1,17 @@
 return {
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    ft = { "markdown" },
-    opts = {},
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    opts = {
+      modes = { "n", "no", "c", "i" },
+      hybrid_modes = { "n", "i" },
+      callbacks = {
+        on_enable = function(_, win)
+          vim.wo[win].conceallevel = 2
+          vim.wo[win].concealcursor = "c"
+        end,
+      },
+    },
   },
   {
     "epwalsh/obsidian.nvim",
