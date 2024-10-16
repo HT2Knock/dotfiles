3d44514f0631d177d36b68f4f103e11f14b5bae3
knock-huynh <huynhtransongngoc@gmail.com>
2024-10-16 21:54:59 +0700

    feat(nvim): add python-ruff pack

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -5,6 +5,7 @@ return {
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.search.nvim-spectre" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
