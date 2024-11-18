b3e9a643bbb90835c88f5bd6904190d90c7de91b
ngochuynh <huynhtransongngoc@gmail.com>
2024-11-18 10:21:56 +0700

    feat(nvim): use supermaven as completion rather than code suggestion

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -2,29 +2,36 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     {
-      "Exafunction/codeium.nvim",
-      config = function() require("codeium").setup {} end,
+      "supermaven-inc/supermaven-nvim",
+      opts = {
+        keymaps = {
+          accept_suggestion = "<C-f>",
+          clear_suggestion = "<Nop>",
+          accept_word = "<C-w>",
+        },
+        log_level = "warn",
+        disable_inline_completion = true, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+        ignore_filetypes = { markdown = true },
+      },
+      config = function(_, opts) require("supermaven-nvim").setup(opts) end,
     },
   },
   opts = function(_, opts)
     opts.formatting.format = require("lspkind").cmp_format {
-      mode = "symbol",
-      maxwidth = 50,
-      ellipsis_char = "...",
-      symbol_map = { Codeium = "" },
+      symbol_map = {
+        Supermaven = "",
+      },
     }
 
     local cmp = require "cmp"
-    -- modify the sources part of the options table
     opts.sources = cmp.config.sources {
+      { name = "supermaven" },
       { name = "nvim_lsp", priority = 750 },
-      { name = "codeium", priority = 770 },
       { name = "luasnip", priority = 725 },
       { name = "buffer", priority = 500 },
       { name = "path", priority = 250 },
     }
-
-    -- return the new table to be used
     return opts
   end,
 }
