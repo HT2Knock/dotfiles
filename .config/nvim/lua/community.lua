d3fed2679d2260235b4895c0a67d76175feb199e
ngochuynh <huynhtransongngoc@gmail.com>
2024-11-27 20:22:52 +0700

    feat(nvim): remove the tabline

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,6 +1,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.recipes.disable-tabline" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.bash" },
