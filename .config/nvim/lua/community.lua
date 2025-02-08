234c8984c0f497a3204ec233d079fe9ca00dfaf3
knock-huynh <huynhtransongngoc@gmail.com>
2025-02-08 16:39:38 +0700

    feat(nvim): add transparent and clean up nvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -2,6 +2,9 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.recipes.disable-tabline" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.bash" },
@@ -9,5 +12,4 @@ return {
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.search.nvim-spectre" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.editing-support.refactoring-nvim" },
 }
