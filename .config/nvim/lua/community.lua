b112d9e59de14f0f8225cefea88735bd1cb29870
T2knock <huynhtransongngoc@gmail.com>
2025-02-14 10:26:14 +0700

    feat(nvim): readded nvim surround turn out i do use it

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -3,6 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.recipes.disable-tabline" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
