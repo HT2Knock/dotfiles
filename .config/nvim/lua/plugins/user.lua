af5dfbf8515faa138e293789dd0bc13858414183
knock-huynh <huynhtransongngoc@gmail.com>
2025-03-22 14:24:35 +0700

    feat(nvim): disable unused plugins

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -7,4 +7,13 @@ return {
     "sphamba/smear-cursor.nvim",
     opts = {},
   },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "vuki656/package-info.nvim",
+    enabled = false,
+  },
+  {
+    "dmmulroy/tsc.nvim",
+    enabled = false,
+  },
 }
