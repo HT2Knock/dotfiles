aca5417e489f4e9a8c6e17e03a05e87c51c01977
T2knock <huynhtransongngoc@gmail.com>
2025-03-25 09:39:33 +0700

    feat(nvim): options smears cursor

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -5,7 +5,14 @@ return {
   },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "dmmulroy/tsc.nvim", enabled = false },
-  { "sphamba/smear-cursor.nvim", opts = {} },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      hide_target_hack = true,
+      cursor_color = "none",
+      legacy_computing_symbols_support = true,
+    },
+  },
   {
     "vuki656/package-info.nvim",
     enabled = false,
