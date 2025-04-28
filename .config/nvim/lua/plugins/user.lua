2787b89b36b2ca00b0d0e3c9ed1d646461c5f1d2
knock-huynh <huynhtransongngoc@gmail.com>
2025-04-28 09:42:47 +0700

    feat(nvim): stop the preselected

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -30,4 +30,16 @@ return {
       { "-", "<cmd>Oil<cr>", desc = "Oil" },
     },
   },
+  {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.completion = opts.completion or {}
+      opts.completion.list = opts.completion.list or {}
+      opts.completion.list.selection = {
+        preselect = false, -- Do not preselect suggestions
+        auto_insert = false, -- Do not auto insert suggestions
+      }
+      return opts
+    end,
+  },
 }
