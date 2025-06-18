f05a779c64d6f7cfe190a77b4816c55eea986e33
knock-huynh <huynhtransongngoc@gmail.com>
2025-06-18 09:55:41 +0700

    feat(nvim): remove the move line keymaps in lazyvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 vim.keymap.set("n", "<leader>k", "i<Enter><Esc>")
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
 
-vim.keymap.del("i", "<A-j>")
-vim.keymap.del("i", "<A-k>")
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
