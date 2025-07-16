7e56383cac4069acb96d571cdc54b6dc9771777a
knock-huynh <huynhtransongngoc@gmail.com>
2025-07-16 15:23:28 +0700

    feat(yazi): use yazi instead of oil

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -7,17 +7,29 @@ return {
   },
   { "folke/flash.nvim", enabled = false },
   {
-    "stevearc/oil.nvim",
-    opts = {
-      default_file_epxlorer = true,
-      view_options = {
-        show_hidden = true,
-      },
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
     },
-    dependencies = { { "echasnovski/mini.icons" } },
-    lazy = false,
     keys = {
-      { "-", "<cmd>Oil<cr>", desc = "Oil" },
+      {
+        "-",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
     },
   },
   {
