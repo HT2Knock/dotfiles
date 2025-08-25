0ac672252495981ce126406495a79185a7b6b187
T2Knock <huynhtransongngoc@gmail.com>
2025-08-25 16:37:18 +0700

    feat(scripts): remove unused script

--- scripts/zsh-history-fix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mv "$HOME"/.local/share/zsh/history "$HOME"/.local/share/zsh/history-bad
-
-strings "$HOME"/.local/share/zsh/history-bad > "$HOME"/.local/share/zsh/history
-
-fc -R "$HOME"/.local/share/zsh/history
-
-rm "$HOME"/.local/share/zsh/history-bad
