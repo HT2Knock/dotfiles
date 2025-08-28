51df4734de6df3c99bc1abcd3b11d99688cf8b6f
T2Knock <huynhtransongngoc@gmail.com>
2025-08-28 11:33:45 +0700

    feat(scripts): change wallpaper script to hyprland

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -1,23 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SOURCE_DIR="$HOME/Pictures/walle/images"
+WALLPAPER_DIR="$HOME/Pictures/walle/images"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
-# Check if SOURCE_DIR exists
-if [ ! -d "$SOURCE_DIR" ]; then
-	echo "Error: Source directory $SOURCE_DIR not found." >&2
-	exit 1
-fi
+# Get a random wallpaper that is not the current one
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
-# Check if nitrogen is installed
-if ! command -v nitrogen &>/dev/null; then
-	echo "Error: Nitrogen is not installed. Please install it and try again." >&2
-	exit 1
-fi
-
-# Get a random wallpaper file
-WALLPAPER=$(find "$SOURCE_DIR" -type f | shuf -n 1)
-
-# Set the same wallpaper for all monitors
-for monitor in {0..1}; do
-	nitrogen --set-auto "$WALLPAPER" --head="$monitor" >/dev/null 2>&1
-done
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
