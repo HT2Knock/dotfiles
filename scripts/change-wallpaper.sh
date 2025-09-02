b4724e8d220f4cf4a8193269bf373cbab01a439e
T2Knock <huynhtransongngoc@gmail.com>
2025-09-02 20:42:56 +0700

    feat(scirpts): add keybind for wallpaper

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/Pictures/walle/images"
-INTERVAL=900 # 15 minutes
 
-while true; do
-    # Generate a randomized list of all files
-    find "$WALLPAPER_DIR" -type f |
-        while read -r img; do
-            echo "$(</dev/urandom tr -dc 0-9 | head -c 8):$img"
-        done |
-        sort -n | cut -d':' -f2- |
-        while read -r img; do
-            [ -n "$img" ] && swww img "$img" \
-                --resize fit \
-                --transition-type center \
-                --transition-fps 60 \
-                --transition-step 2
-            sleep "$INTERVAL"
-        done
-done
+# Generate a randomized list of all files and select one
+img=$(find "$WALLPAPER_DIR" -type f |
+    while read -r file; do
+        echo "$(</dev/urandom tr -dc 0-9 | head -c 8):$file"
+    done |
+    sort -n | cut -d':' -f2- | head -n 1)
+
+# Set the wallpaper if an image was found
+[ -n "$img" ] && swww img "$img" --transition-type center
