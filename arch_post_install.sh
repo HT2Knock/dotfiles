9c2900810f17551b26d21ab2866aa7af9de0e6b3
T2Knock <huynhtransongngoc@gmail.com>
2025-09-03 18:44:07 +0700

    feat(cliphist): use as clipboard manager combine with fuzzel

--- arch_post_install.sh
+++ arch_post_install.sh
@@ -23,7 +23,7 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm \
     base-devel unzip curl wget git fastfetch openssh wl-clipboard \
     gcc clang go \
-    neovim zsh ghostty rofi nautilus nwg-look \
+    neovim zsh ghostty nautilus nwg-look fuzzel cliphist \
     lazygit lazydocker \
     tmux ffmpeg p7zip jq poppler fzf \
     imagemagick stow \
