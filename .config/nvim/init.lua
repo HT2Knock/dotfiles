7580d41c16ffc44f485391f6164184394f112f60
T2Knock <huynhtransongngoc@gmail.com>
2025-09-28 10:10:05 +0700

    nvim: set filetype extension for .env type

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,7 +6,15 @@ require 'config.autocommands'
 
 -- Setting
 vim.cmd [[colorscheme tokyonight]]
-vim.filetype.add { extension = { ['http'] = 'http' } }
+vim.filetype.add {
+  extension = {
+    http = 'http',
+    env = 'conf',
+  },
+  pattern = {
+    ['.*%.env%..+'] = 'conf',
+  },
+}
 
 -- Global winbar bg
 vim.api.nvim_set_hl(0, 'WinBar', { bg = '#1a1b26' })
