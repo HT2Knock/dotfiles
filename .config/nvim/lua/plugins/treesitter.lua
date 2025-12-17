b1d371d2f017b912360ea1febc926a622ff8c3ba
T2Knock <huynhtransongngoc@gmail.com>
2025-12-17 14:24:59 +0700

    tmux: set the key without double tap

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -42,6 +42,5 @@ return {
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
   --
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
   --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
