00135cb5e3dd12274004182fc9d7ba2d07bfe132
T2Knock <huynhtransongngoc@gmail.com>
2026-01-18 22:02:24 +0700

    nvim: config nvim surround

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -5,13 +5,15 @@ return {
     version = '*',
     opts = {
       mappings = {
-        add = 'sa', -- Add surrounding (normal mode)
-        delete = 'sd', -- Delete surrounding
-        find = 'sf', -- Find left surrounding
-        find_right = 'sF', -- Find right surrounding
+        add = 'sa', -- Add surrounding in Normal and Visua modes
+        deete = 'sd', -- Delete surrounding
+        find = 'sf', -- Find surrounding (to the right)
+        find_left = 'sF', -- Find surrounding (to the left)
         highlight = 'sh', -- Highlight surrounding
         replace = 'sr', -- Replace surrounding
-        update_n_lines = 'sn', -- Update `n_lines`
+
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
       },
     },
   },
