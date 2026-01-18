5a7fdb6602812a2b23467dee2e3f8a2a41bfda15
T2Knock <huynhtransongngoc@gmail.com>
2026-01-18 16:55:21 +0700

    nvim: add treesitter-textobjects, mini.surround, and restore settings

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,1 +1,18 @@
-return { { 'nvim-mini/mini.icons', version = '*', opts = {} } }
+return {
+  { 'nvim-mini/mini.icons', version = '*', opts = {} },
+  {
+    'echasnovski/mini.surround',
+    version = '*',
+    opts = {
+      mappings = {
+        add = 'sa', -- Add surrounding (normal mode)
+        delete = 'sd', -- Delete surrounding
+        find = 'sf', -- Find left surrounding
+        find_right = 'sF', -- Find right surrounding
+        highlight = 'sh', -- Highlight surrounding
+        replace = 'sr', -- Replace surrounding
+        update_n_lines = 'sn', -- Update `n_lines`
+      },
+    },
+  },
+}
