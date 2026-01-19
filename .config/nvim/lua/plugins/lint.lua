58f49da68598940ff6f14ae6c1da834ba153e43d
T2Knock <huynhtransongngoc@gmail.com>
2026-01-19 20:57:47 +0700

    nvim: fix correct lint name

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
 
     lint.linters_by_ft = {
       python = { 'ruff' },
-      go = { 'golangci-lint' },
+      go = { 'golangcilint' },
 
       sh = { 'shellcheck' },
       bash = { 'shellcheck' },
