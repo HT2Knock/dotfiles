61a547530b47dc34386b5a25e782e4eec7934e21
T2Knock <huynhtransongngoc@gmail.com>
2026-05-25 08:29:56 +0700

    nvim: add hint using hardtime nvim

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,12 @@
 return {
+  {
+    'm4xshen/hardtime.nvim',
+    lazy = false,
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {
+      restriction_mode = 'hint',
+    },
+  },
   { 'folke/todo-comments.nvim', opts = {} },
   {
     'esmuellert/vscode-diff.nvim',
