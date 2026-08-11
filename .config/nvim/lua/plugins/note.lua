6a6b6fe590d1b0ccf1272be12fe1aa95ce5c224f
HT2Knock <ngoc.hts@airrlabs.com>
2026-08-11 14:20:54 +0700

    nvim: add markdown preview

--- .config/nvim/lua/plugins/note.lua
+++ .config/nvim/lua/plugins/note.lua
@@ -1,5 +1,14 @@
 return {
   {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+  {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },
     opts = {},
