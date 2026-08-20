2913e00e431d94e6da1f22d63d53829e22a539d8
HT2Knock <ngoc.hts@airrlabs.com>
2026-08-20 18:33:51 +0700

    nvim: disable snacks scroll

--- .config/nvim/lua/plugins/snack.lua
+++ .config/nvim/lua/plugins/snack.lua
@@ -384,7 +384,6 @@ return {
     quickfile = { enabled = true },
     rename = { enabled = true },
     health = { enabled = true },
-    scroll = { enabled = true },
     image = {
       enabled = true,
       resolve = function(path, src)
